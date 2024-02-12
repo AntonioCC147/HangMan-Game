1d4e515c50a71fa2cc34af00f784f14d7f6fa624
AntonioCC147 <77618117+AntonioCC147@users.noreply.github.com>
2024-02-12 20:46:15 +0200

    Update game.js

--- frontend/src/pages/game/game.js
+++ frontend/src/pages/game/game.js
@@ -77,7 +77,7 @@ export default function Game() {
         }
     }
     
-    const [time, setTime] = useState(300);
+    const [time, setTime] = useState(150);
     useEffect(() => {
         const timerId = setInterval(countdown, 1000);
 
